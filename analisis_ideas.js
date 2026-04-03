861b9a96f219f8f33f44c934ca16c8d737a6befb
yariancn <yarianc@yahoo.com>
2026-04-03 08:59:07 -0500

    Delete analisis_ideas.js

--- analisis_ideas.js
+++
@@ -1,8 +0,0 @@
-// analisis_ideas.js - VALIDACIÓN DE MODELOS DE NEGOCIO
-const { REGLAS_NUCLEARES } = require('./directrices');
-
-const PROMPTS_IDEAS = {
-  VALIDACION: (idea) => `${REGLAS_NUCLEARES}\n### ANÁLISIS DE VIABILIDAD\n¿Por qué esta idea va a fallar si no se cambia la estructura? Analiza la demanda real. Idea: ${idea}`,
-  ESTRUCTURA: (idea) => `### ARQUITECTURA DEL MODELO\nPasos para que el negocio nazca sin fricción y genere capital desde el día 1. Idea: ${idea}`
-};
-module.exports = { PROMPTS_IDEAS };
